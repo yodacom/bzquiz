4820e4ff32db0ed857c4271c09ea8544c6ec6b6b
Jeremy Black <jb@yodacom.com>
2017-01-06 15:34:41 -0700

    continued to add to gulp file but still need live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,41 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-gulp.task('styles', function(){
-    gulp.src('./css/style.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('./css/'));
+var gulp = require('gulp'),
+	stylus = require('gulp-stylus'),
+	livereload = require('gulp-livereload'),
+	uglify = require('gulp-uglify'); 
+
+// Error Log function
+function errorLog(error) {
+	console.error.bind(error);
+	this.emit('end');
+}
+
+// SCRIPTS TASKS
+//Uglifies
+
+gulp.task('scripts', function(){
+	gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
 
+// Styles Task
+gulp.task('styles', function(){
+	gulp.src('./css/style.styl')
+        .pipe(stylus())
+        .on('error', errorLog)
+        .pipe(gulp.dest('./css/'))
+        .pipe(livereload());
+});
+
+// Watch Styles Task
 gulp.task('watch:styles', function(){
-        gulp.watch('**/*.styl', ['styles']);
-        });
+	var server = livereload();
+	gulp.watch('**/*.styl', ['styles']);
+});
+
+// Watch Task
+gulp.task('watch', function(){
+	gulp.watch('js/*.js', ['scripts']);
+});
+
+gulp.task ('default', ['scripts', 'watch:styles']);
