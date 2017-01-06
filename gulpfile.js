d076558b541155ef61bcf0f3cf3e0442ad2e5ab0
Jeremy Black <jb@yodacom.com>
2017-01-06 06:49:22 -0700

    added use of Yarn Stylus and Gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+gulp.task('styles', function(){
+    gulp.src('style.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch:styles', function(){
+        gulp.watch('**/*.styl', ['styles']);
+        });
