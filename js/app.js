a80af1f29e764b4c18e2f61a6af0c9bb70945b44
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-02-10 12:06:01 -0400

    Modify app.js and index.html to use the new documents module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 require('../styles/main.styl');
 var $ = require('jquery');
+var documents = require('./documents');
 
 var beerList =[];
 var selectedBeer = null;
@@ -130,71 +131,6 @@ function loadBeer(beerId) {
 	var url = "http://api.brewerydb.com/v2/search?q=fat tire&type=beer&key=758086eed7b9e97ad41b21d18b37b4a7";
 }
 
-	// beerList = data.items;
-
-
-
-	// $("#textContainer").append(output);
-
-	// Build output
-
-    /*
-     var item = {
-     "id": "vJCXKD",
-     "name": "Coors Banquet",
-     "nameDisplay": "Coors Banquet",
-     "description": "Coors, nicknamed the \"Banquet Beer,\" was first introduced by Adolph Coors 1873. According to legend, thirsty miners in the late 1800s threw celebratory banquets with Coors as the honorary beer because of its superior craftsmanship. Prior to its nationwide distribution in 1981, Coors built a cult following, with presidents, movie stars and many others making special trips out west to buy it. Coors is brewed in the Rockies for a uniquely crisp, clean and drinkable taste.",
-     "abv": "5",
-     "glasswareId": 5,
-     "availableId": 1,
-     "styleId": 94,
-     "isOrganic": "N",
-     "labels": {
-     "icon": "https://s3.amazonaws.com/brewerydbapi/beer/vJCXKD/upload_E7cekQ-icon.png",
-     "medium": "https://s3.amazonaws.com/brewerydbapi/beer/vJCXKD/upload_E7cekQ-medium.png",
-     "large": "https://s3.amazonaws.com/brewerydbapi/beer/vJCXKD/upload_E7cekQ-large.png"
-     },
-     "status": "verified",
-     "statusDisplay": "Verified",
-     "createDate": "2012-01-03 02:43:01",
-     "updateDate": "2015-12-15 23:20:59",
-     "glass": {
-     "id": 5,
-     "name": "Pint",
-     "createDate": "2012-01-03 02:41:33"
-     },
-     "available": {
-     "id": 1,
-     "name": "Year Round",
-     "description": "Available year round as a staple beer."
-     },
-     "style": {
-     "id": 94,
-     "categoryId": 8,
-     "category": {
-     "id": 8,
-     "name": "North American Lager",
-     "createDate": "2012-03-21 20:06:46"
-     },
-     "name": "American-Style Light (Low Calorie) Lager",
-     "shortName": "American Light Lager",
-     "description": "These beers are extremely light colored, light in body, and high in carbonation. Calorie level should not exceed 125 per 12 ounce serving. Corn, rice, or other grain or sugar adjuncts are often used. Flavor is mild and hop bitterness and aroma is negligible to very low. Light fruity esters are acceptable. Chill haze and diacetyl should be absent.",
-     "ibuMin": "5",
-     "ibuMax": "10",
-     "abvMin": "3.5",
-     "abvMax": "4.4",
-     "srmMin": "2",
-     "srmMax": "4",
-     "ogMin": "1.024",
-     "fgMin": "1.002",
-     "fgMax": "1.008",
-     "createDate": "2012-03-21 20:06:46",
-     "updateDate": "2015-04-07 15:39:35"
-     },
-     "type": "beer"
-     }
-     */
-
 function getOutput(item) {
 	var beerID = item.id;
 	var beerName = item.nameDisplay;
@@ -270,10 +206,9 @@ $(function() {
 // ===== Modal Window functions ========
 	$(".modalTrigger").click(function(e){
 		e.preventDefault();
-		$.get("js/documents/" + $(this).data("page"))
-        .done(function(data){
-	        $("#contents").html(data);
-});
+
+        $("#contents").html(documents[$(this).data("page")]);
+
 		$(".modal").show();
 	});
 
@@ -416,24 +351,10 @@ function compare(){
 	// Calculate Results and show text
 
 	function quizFinalResults (numCorrect) {
-		if (numCorrect == 0) {
-			$.get("./js/documents/keepTasting.html", function (data){
-				$("#resultsText").html(data);
-			});
-
-		} else if (numCorrect == 1) {
-			$.get("./js/documents/gettingThere.html", function (data){
-				$("#resultsText").html(data);
-			})
-		}
-			else if (numCorrect == 2) {
-			$.get("./js/documents/gettingThere.html" , function (data){
-				$("#resultsText").html(data);
-			});
-			}
-			else {
-			$.get("./js/documents/youWin.html", function (data){
-				$("#resultsText").html(data);
-			})
-	}
+        switch(numCorrect){
+            case 0: $("#resultsText").html(documents.keepTasting); break;
+            case 1:
+            case 2: $("#resultsText").html(documents.gettingThere); break;
+            default: $("#resultsText").html(documents.youWin); break;
+        }
 }
